c88e1d247d49a571d11b65daa1c5d1e4eae74990
Rois <roisbks@gmail.com>
2024-04-03 17:12:22 +0700

    initial commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+  public function index()
+  {
+    echo "<h1>Perkenalkan</h1>";
+    echo"Nama saya M. Rois  Saya tinggal di daerah tarumajaya olah raga yang saya sukai adalah gatau"; 
+  }
+}
