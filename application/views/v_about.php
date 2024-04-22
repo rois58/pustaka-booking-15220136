e6de2edb70094121b510b173e146d717ca949b43
Rois <roisbks@gmail.com>
2024-04-22 18:27:09 +0700

    au

---
+++ application/views/v_about.php
@@ -0,0 +1,21 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="none">
+        Nama : Mohamad Rois Alfariji
+        
+    </ul>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Rosma cipta indah 2</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Cilcap</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Catur</li>
+    </ul>
+</section>
